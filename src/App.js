64d1b23a783c2645554ff7dd91ed6439bf43a16e
Jex <jexdelossantos2010@gmail.com>
2021-05-03 06:50:31 +0800

    Graphic Design(GD): Installed Tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="flex w-full">
+        <div>HELLO</div>
+        <div>THERE</div>
+      </div>
     </div>
   );
 }

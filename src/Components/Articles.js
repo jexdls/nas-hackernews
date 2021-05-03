09b8fe5c2a1aac1a9dab22369e7df14b11c50ec2
Jex <jexdelossantos2010@gmail.com>
2021-05-03 19:57:28 +0800

    Function: sorted items from new to old

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,6 +1,11 @@
 import ArticleCard from "./ArticlesComponents/ArticleCard";
 
 export default function Articles({ articleItems }) {
+  // B takes precedence before A when sorting. So, sorting method is from low to high
+  articleItems.sort((a, b) =>
+    parseInt(a.timePassed) > parseInt(b.timePassed) ? 1 : -1
+  );
+
   return (
     <div>
       {/* Map through the articles JSON and generate a component for each article  */}
